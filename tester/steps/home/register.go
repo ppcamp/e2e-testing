b64ec661224fc76db6197c02aa41a35814d232b5
ppcamp <p.augustocampos@gmail.com>
2023-05-08 01:06:49 -0300

    fix: test

--- tester/steps/home/register.go
+++ tester/steps/home/register.go
@@ -7,7 +7,7 @@ import (
 
 func Register(ctx *godog.ScenarioContext) {
 	ctx.Step(`^I launch the home page$`, support.WithReporter(launchHomePage))
-	ctx.Step(`^I enter with text globo$`, support.WithReporter(enterWithText))
+	ctx.Step(`^I enter with text globo$`, support.WithReporter(enterWithTextGlobo))
 	ctx.Step(`^I enter with text msn$`, support.WithReporter(enterWithText))
 	ctx.Step(`^I hit the search button$`, support.WithReporter(clickButton))
 	ctx.Step(`^Verify the page title`, support.WithReporter(verifyTitle))
